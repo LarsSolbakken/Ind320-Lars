68290a160f360e7c4b1b770b23fc6805ecbc1820
Lars Solbakken <lars.johan.tveit.solbakken@nmbu.no>
2025-10-28 14:43:22 +0100

    WIP: local edits before rebase

--- Project work, part 2/notes.py
+++ Project work, part 2/notes.py
@@ -228,3 +228,82 @@
 #                    .withColumnRenamed("quantityKwh", "quantitykwh")
 # df_spark.printSchema()
 # df_spark.show(5)
+
+
+
+
+
+
+######streamlitapp
+ # if df_month.empty:
+    #     st.warning("No data available for this selection.")
+    # else:
+    #     fig2, ax2 = plt.subplots(figsize=(8, 6))
+    #     sns.lineplot(data=df_month, x="starttime", y="quantitykwh", hue="productiongroup", ax=ax2)
+    #     month_names = [pd.to_datetime(str(m), format="%m").strftime("%B") for m in selected_month]
+    #     ax2.set_title(f"Production in {selected_area}, {', '.join(month_names)} 2021")
+    #     # ax2.set_title(f"Production in {selected_area}, {pd.to_datetime(str(selected_month), format='%m').strftime('%B')} 2021")
+    #     ax2.set_xlabel("Date")
+    #     ax2.set_ylabel("kWh")
+    #     st.pyplot(fig2)
+
+    #     summary = df_month.groupby("productiongroup")["quantitykwh"].sum().reset_index()
+    #     summary["quantitykwh"] = (summary["quantitykwh"]/1e6).round(2)  # convert to GWh
+    #     st.write("### Monthly Summary (GWh)")
+    #     st.dataframe(summary)
+
+# months = list(range(1, 13))
+    # selected_month = st.multiselect("Select Month", months, format_func=lambda x: pd.to_datetime(str(x), format="%m").strftime("%B"))
+    
+    
+    # df_month = df[
+    #     (df["pricearea"] == selected_area) &
+    #     (df["productiongroup"].isin(selected_groups)) &
+    #     (df["starttime"].dt.month.isin(selected_month))
+    # ]
+
+
+
+# /* global use, db */
+# // MongoDB Playground
+# // To disable this template go to Settings | MongoDB | Use Default Template For Playground.
+# // Make sure you are connected to enable completions and to be able to run a playground.
+# // Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+# // The result of the last command run in a playground is shown on the results panel.
+# // By default the first 20 documents will be returned with a cursor.
+# // Use 'console.log()' to print to the debug output.
+# // For more documentation on playgrounds please refer to
+# // https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
+
+# // Select the database to use.
+# use('mongodbVSCodePlaygroundDB');
+
+# // Insert a few documents into the sales collection.
+# db.getCollection('sales').insertMany([
+#   { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
+#   { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
+#   { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
+#   { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
+#   { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
+#   { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
+#   { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
+#   { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
+# ]);
+
+# // Run a find command to view items sold on April 4th, 2014.
+# const salesOnApril4th = db.getCollection('sales').find({
+#   date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
+# }).count();
+
+# // Print a message to the output window.
+# console.log(`${salesOnApril4th} sales occurred in 2014.`);
+
+# // Here we run an aggregation and open a cursor to the results.
+# // Use '.toArray()' to exhaust the cursor to return the whole result set.
+# // You can use '.hasNext()/.next()' to iterate through the cursor page by page.
+# db.getCollection('sales').aggregate([
+#   // Find all of the sales that occurred in 2014.
+#   { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
+#   // Group the total sales for each product.
+#   { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
+# ]);
